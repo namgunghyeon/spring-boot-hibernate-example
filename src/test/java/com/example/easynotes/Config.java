9a2cac4ca363fe1f544e568623719965dbcf98e7
namgunghyeon <nkh@recobell.com>
2019-04-02 16:25:06 +0900

    feat: repository insert 추가

--- src/test/java/com/example/easynotes/Config.java
+++ src/test/java/com/example/easynotes/Config.java
@@ -1,4 +1,23 @@
 package com.example.easynotes;
 
+import com.example.easynotes.config.TestConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
 public class Config {
+
 }
--- src/test/java/com/example/easynotes/JPATest.java
+++ src/test/java/com/example/easynotes/JPATest.java
@@ -7,11 +7,17 @@ import com.example.easynotes.model.Order;
 import com.example.easynotes.repository.BookCategoryRepository;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class JPATest {
     @Autowired
     private BookCategoryRepository bookCategoryRepository;
@@ -28,6 +34,8 @@ public class JPATest {
         bookCategory.setBooks(new HashSet<>(Arrays.asList(book1)));
 
         Assert.assertEquals("Check Order size",1, bookCategory.getBooks().size());
+        System.out.println(bookCategoryRepository);
+        bookCategoryRepository.save(bookCategory);
     }
 
     @Test
